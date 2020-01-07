34133b0e3621e3a17048eae17ee3d2a5e151964b
Felipe Bellini <Fe.Bell@outlook.com>
2020-01-07 10:39:59 -0500

    Pushing original source

---
+++ RapidXML/rapidxml_ext.h
@@ -0,0 +1,53 @@
+/***********************************************************************
+* Project           : Game
+* Author            : Felipe Bellini
+* Date created      : Oct. 12 - 2019
+* Licensed under:	: MIT
+
+************************************************************************/
+
+/***********************************************************************
+* Project           : XML_IO @ rapidxml_ext.h
+* Program name      : XML_IO.exe
+* Author            : Felipe Bellini
+* Date created      : Sep. 27 - 2019
+* Purpose           : Solves some RapidXml issues with newer versions of GCC:
+					  https://stackoverflow.com/questions/14113923/rapidxml-print-header-has-undefined-methods
+************************************************************************/
+#ifndef RAPIDXML_EXT_H_
+#define RAPIDXML_EXT_H_
+#include "rapidxml.hpp"
+/* Adding declarations to make it compatible with gcc 4.7 and greater */
+namespace rapidxml {
+	namespace internal {
+		template <class OutIt, class Ch>
+		inline OutIt print_children(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+
+		template <class OutIt, class Ch>
+		inline OutIt print_attributes(OutIt out, const xml_node<Ch>* node, int flags);
+
+		template <class OutIt, class Ch>
+		inline OutIt print_data_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+
+		template <class OutIt, class Ch>
+		inline OutIt print_cdata_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+
+		template <class OutIt, class Ch>
+		inline OutIt print_element_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+
+		template <class OutIt, class Ch>
+		inline OutIt print_declaration_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+
+		template <class OutIt, class Ch>
+		inline OutIt print_comment_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+
+		template <class OutIt, class Ch>
+		inline OutIt print_doctype_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+
+		template <class OutIt, class Ch>
+		inline OutIt print_pi_node(OutIt out, const xml_node<Ch>* node, int flags, int indent);
+	}
+}
+#include "rapidxml_print.hpp"
+
+#endif /* RAPIDXML_EXT_H_ */
